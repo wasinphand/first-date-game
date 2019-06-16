9d1ca13b8997cda1f0cc3fe19324c79a73c00782
wasinphand <46191169+wasinphand@users.noreply.github.com>
2019-06-16 17:41:38 +0700

    Add files via upload

---
+++ precache-manifest.1fd20fbec397e12ccebee2997e2fff8a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6459a18a6fab02f7ae92fd7b9e1ef96c",
+    "url": "/static/media/nyancat.6459a18a.gif"
+  },
+  {
+    "revision": "bcc90165bc9e32169ef05e5ff450c7d0",
+    "url": "/static/media/eng1.bcc90165.jpg"
+  },
+  {
+    "revision": "64ee67a9a7af34a8bd3429d96bc0e0e3",
+    "url": "/static/media/dhammy.64ee67a9.jpg"
+  },
+  {
+    "revision": "63f453eb0582866b6f6ead24c337028e",
+    "url": "/static/media/KRR-THAISPIRIT.63f453eb.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d1e43b449a3a4517d5d",
+    "url": "/static/js/main.437f70cd.chunk.js"
+  },
+  {
+    "revision": "2a4703e2457410af112e",
+    "url": "/static/js/2.8395228b.chunk.js"
+  },
+  {
+    "revision": "5d1e43b449a3a4517d5d",
+    "url": "/static/css/main.2a2e9408.chunk.css"
+  },
+  {
+    "revision": "2a4703e2457410af112e",
+    "url": "/static/css/2.81f07c25.chunk.css"
+  },
+  {
+    "revision": "04b910035b185f517a14566aed25bab6",
+    "url": "/index.html"
+  }
+];
